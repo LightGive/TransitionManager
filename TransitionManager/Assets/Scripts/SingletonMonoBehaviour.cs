c90b26eb9fafd32a937da2d377205790d26ebcf9
LightGive <roi.akase@outlook.jp>
2018-02-03 23:54:59 +0900

    シングルトン追加

---
+++ TransitionManager/Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace LightGive
+{
+	public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static T instance;
+		public static T Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = (T)FindObjectOfType(typeof(T));
+					if (instance == null)
+					{
+						Debug.LogError(typeof(T) + "is nothing");
+					}
+				}
+				return instance;
+			}
+		}
+
+		protected virtual void Awake()
+		{
+			CheckInstance();
+		}
+
+		protected bool CheckInstance()
+		{
+			if (this == Instance) { return true; }
+			Destroy(this);
+			return false;
+		}
+	}
+}
